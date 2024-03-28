1a420437edb7b957b4e2116a16fcfeefb2a356a0
Hamza-Aziz <Hamza-Aziz@users.noreply.github.com>
2024-03-28 14:59:38 +0000

    jekyll build from Action 34dcadfca56fe5c865f7b9f1a88d96cbba6a7318

---
+++ Readme.md
@@ -0,0 +1,5 @@
+- to add or change a navigation bar item go to _data/navigation.yml
+    - adding a new navigation bar
+      - the `permalink:` attribute on the `_pages/file.md` should be the same as the `url` attribute on navigation.yml file
+- layout folder files are like templetes that gives your pages files a form
+  
---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "Understand Terraform import with AWS IAM users and groups",
+        "excerpt":"How to import a resource created manually under Terraform management To avoid configuration drift, Terraform has importing capabilities for most of the AWS resources. By importing a resource, Terraform stores in its state file the setup of the resource, as it’s currently on the cloud. So, when you run terraform...","categories": ["Terraform","AWS"],
+        "tags": [],
+        "url": "/terraform/aws/understand-terraform-import-wiht-aws/",
+        "teaser": null
+      },{
+        "title": "Kubernetes CNI plugin vs Kube-proxy",
+        "excerpt":"Before discussing the difference between the two components, let’s take a second to define the CNI, What is the CNI ? CNI (Container Network Interface),is a Cloud Native Computing Foundation project, consists of a specification and libraries for writing plugins to configure network interfaces in Linux containers, it concerns itself...","categories": ["k8s"],
+        "tags": [],
+        "url": "/k8s/kube-proxy-cni-plugin-in-k8s/",
+        "teaser": null
+      },{
+        "title": "Managing Multiple Environments using Terraform Workspace vs Folders vs Terragrunt",
+        "excerpt":"As your infrastructure grows, managing multiple environments (such as development, staging, and production) can become challenging. In this blog post, we’ll explore three popular approaches in Terraform open source version to managing multiple environments: Terraform workspace, folder structure, and Terragrunt. Terraform Workspace Terraform workspace is a built-in feature of Terraform...","categories": ["Terraform"],
+        "tags": [],
+        "url": "/terraform/Terraform-workspace-terragrunt/",
+        "teaser": null
+      },{
+        "title": "Hardlink vs Softlink",
+        "excerpt":"In this article, we’ll talk about the difference between soft links and hard links, they might sounds simple and easy (in fact they are) but many engineers still don’t know the main difference between the two types, To understand softlink and hardlink, we need to understand first Inode in Linux...","categories": ["Linux"],
+        "tags": [],
+        "url": "/linux/Hardlink-vs-softlink/",
+        "teaser": null
+      },{
+        "title": "Storage and Workload Separation in Kubernetes with Longhorn",
+        "excerpt":"Context Let’s say you have a k8s or a k3s cluster composed of 6 worker nodes, and for some reasons, you want to separate compute from storage in a way that some worker nodes run only Longhorn pods and take care of storage (volumes, replicas), while the rest of the...","categories": ["k8s","longhorn","k3s"],
+        "tags": [],
+        "url": "/k8s/longhorn/k3s/Storage-and-Workload-Separation-in-Kubernetes-with-longhorn/",
+        "teaser": null
+      }]
